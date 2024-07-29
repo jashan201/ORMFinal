76a1a6ad26cfa9208b67c3d5a7396e2defd9ed1a
grlinski <gabriellinski@student.mitt.ca>
2024-07-29 11:03:31 -0500

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ORMFinal.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
